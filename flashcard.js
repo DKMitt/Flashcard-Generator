77a76f10387a5b0745fa1f773cf02bf4cd3030cd
DKMitt <dkmitt@sbcglobal.net>
2017-04-11 21:06:14 -0500

    Started Pseudo Coding

---
+++ flashcard.js
@@ -0,0 +1,25 @@
+
+// Pseudo Coding
+// Create a BasicCard constructor. It should accept front and back arguments.
+
+
+// Create a ClozeCard constructor. It should accept text and cloze arguments.
+
+
+// ClozeCard should have a property or method that contains or returns only the cloze-deleted portion of the text.
+
+
+// ClozeCard should have a property or method that contains or returns only the partial text.
+
+
+// ClozeCard should have a property or method that contains or returns only the full text.
+
+
+// ClozeCard should throw or log an error when the cloze deletion does not appear in the input text.
+
+
+// Use prototypes to attach these methods, wherever possible.
+
+
+// As your application will not have a front end, your only need to determine an efficient way to store cloze-deleted cards—you don't have to solve the problem of displaying them. You are free to decide how you'd like to implement this. One might represent the above flashcard thus:
+

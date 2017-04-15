9511df9fa76b9f9844e42e05beec2305cbbb6b0b
Darryl Mittlestaedt <dkm.ncbc@gmail.com>
2017-04-15 01:32:46 -0500

    manual file upload

---
+++ data.js
@@ -0,0 +1,27 @@
+console.log(' Front & Back of Cards Have Loaded!');
+
+exports.basicCards = {
+  frontCardOne: 'Who was the first president of the United States?',
+  backCardOne: 'George Washington'
+},
+{
+  frontCardTwo: 'Who was the youngest president?',
+  backCardTwo: 'Theodore Roosevelt, He became president in 1901 @ Age 42.'
+},
+{
+  frontCardTwo: 'Which president of the united states dated Marilyn Monroe?',
+  backCardTwo: 'John F. Kennedy'
+}
+
+exports.clozeCards = {
+  frontCardOne: '...was the first Batman.',
+  backCardOne: 'Adam West was the first Batman.'
+},
+{
+  frontCardOne: '... played the first James Bond, 007.',
+  backCardOne: 'Barry Nelson played the first James Bond, 007.'
+},
+{
+  frontCardOne: '... played the first Dr. Who in 1963.',
+  backCardOne: 'William Hartnell played the first Dr. Who in 1963.'
+}
--- flashcard.js
+++ flashcard.js
@@ -1,25 +1,68 @@
 
 // Pseudo Coding
 // Create a BasicCard constructor. It should accept front and back arguments.
-
-
 // Create a ClozeCard constructor. It should accept text and cloze arguments.
-
-
 // ClozeCard should have a property or method that contains or returns only the cloze-deleted portion of the text.
-
-
 // ClozeCard should have a property or method that contains or returns only the partial text.
-
-
 // ClozeCard should have a property or method that contains or returns only the full text.
-
-
 // ClozeCard should throw or log an error when the cloze deletion does not appear in the input text.
-
-
 // Use prototypes to attach these methods, wherever possible.
-
-
 // As your application will not have a front end, your only need to determine an efficient way to store cloze-deleted cards—you don't have to solve the problem of displaying them. You are free to decide how you'd like to implement this. One might represent the above flashcard thus:
 
+var key = require('./data.js');
+var inquirer = require('inquirer');
+var fs = require('fs');
+
+var cmdInput = process.argv[2];
+
+getInput(cmdInput);
+
+function getInput(cmdInput, args) {
+    if (logged()) {
+        switch (cmdInput) {
+        case 'basic':
+            console.log(" Basic Cards Display Here!");
+            basicCards();
+            break;
+        case 'cloze':
+            console.log(' Cloze Cards Display Here!');
+            clozeCards();
+            break;
+        }
+    }
+}
+
+
+function basicCards(argument) {
+	// basic code goes here
+}
+
+
+
+function clozeCards(argument) {
+	// cloze code goes here
+}
+
+
+
+
+
+
+
+
+
+function logged() {
+    // captures all command line inputs
+    var inputs = process.argv.slice(2).join(" ");
+    // feeeds the  data to the log file
+    // appends data to the log file after each run
+    fs.appendFile("log.txt", "node flashcard.js: " + inputs + "\n", function (error) {
+        if (error) {
+            throw error;
+        }
+        else {
+            console.log(" The log file was updated! ");
+        }
+    });
+    return true;
+}
